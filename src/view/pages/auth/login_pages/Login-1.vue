54e2aa54de938fdc0f42875b4f66e5bd6329b53f
Furkan Arslan <Furkan.Arslan@vodafone.com>
2020-10-30 11:02:06 +0300

    login page changes

--- src/view/pages/auth/login_pages/Login-1.vue
+++ src/view/pages/auth/login_pages/Login-1.vue
@@ -17,8 +17,8 @@
         <div class="d-flex flex-column-auto flex-column pt-lg-40 pt-15">
           <a href="#" class="text-center mb-10">
             <img
-              src="media/logos/logo-letter-1.png"
-              class="max-h-70px"
+              src="media/logos/kandilli-logo.png"
+              class="max-h-120px"
               alt=""
             />
           </a>
@@ -26,11 +26,11 @@
             class="font-weight-bolder text-center font-size-h4 font-size-h1-lg"
             style="color: #986923;"
           >
-            Discover Amazing Metronic <br />with great build tools
+            Kandilli Pastanesi <br /> Randevu Uygulaması
           </h3>
         </div>
         <div
-          class="aside-img d-flex flex-row-fluid bgi-no-repeat bgi-position-y-bottom bgi-position-x-center"
+          class="aside-img d-flex flex-row-fluid bgi-size-contain bgi-position-x-center bgi-position-y-center"
           :style="{ backgroundImage: `url(${backgroundImage})` }"
         ></div>
       </div>
@@ -52,17 +52,8 @@
                 <h3
                   class="font-weight-bolder text-dark font-size-h4 font-size-h1-lg"
                 >
-                  Welcome to Metronic
+                  Hoş Geldiniz
                 </h3>
-                <span class="text-muted font-weight-bold font-size-h4"
-                  >New Here?
-                  <a
-                    id="kt_login_signup"
-                    class="text-primary font-weight-bolder"
-                    @click="showForm('signup')"
-                    >Create an Account</a
-                  ></span
-                >
               </div>
               <div class="form-group">
                 <label class="font-size-h6 font-weight-bolder text-dark"
@@ -85,13 +76,13 @@
               <div class="form-group">
                 <div class="d-flex justify-content-between mt-n5">
                   <label class="font-size-h6 font-weight-bolder text-dark pt-5"
-                    >Password</label
+                    >Şifre</label
                   >
                   <a
                     class="text-primary font-size-h6 font-weight-bolder text-hover-primary pt-5"
                     id="kt_login_forgot"
                     @click="showForm('forgot')"
-                    >Forgot Password ?</a
+                    >Şifreyi mi Unuttunuz ?</a
                   >
                 </div>
                 <div
@@ -114,108 +105,12 @@
                   ref="kt_login_signin_submit"
                   class="btn btn-primary font-weight-bolder font-size-h6 px-15 py-4 my-3 mr-3"
                 >
-                  Sign In
-                </button>
-                <button
-                  type="button"
-                  class="btn btn-light-primary font-weight-bolder px-8 py-4 my-3 font-size-lg"
-                >
-                  <span class="svg-icon svg-icon-md">
-                    <inline-svg
-                      src="media/svg/social-icons/google.svg"
-                    /> </span
-                  >Sign in with Google
+                  Giriş Yap
                 </button>
               </div>
             </form>
           </div>
           <!--end::Signin-->
-          <!--begin::Signup-->
-          <div class="login-form login-signup">
-            <form
-              class="form"
-              novalidate="novalidate"
-              id="kt_login_signup_form"
-              @submit.stop.prevent="onSubmitRegister()"
-            >
-              <div class="pb-13 pt-lg-0 pt-5">
-                <h3
-                  class="font-weight-bolder text-dark font-size-h4 font-size-h1-lg"
-                >
-                  Sign Up
-                </h3>
-                <p class="text-muted font-weight-bold font-size-h4">
-                  Enter your details to create your account
-                </p>
-              </div>
-              <div class="form-group">
-                <input
-                  class="form-control form-control-solid h-auto py-7 px-6 rounded-lg font-size-h6"
-                  type="text"
-                  placeholder="Fullname"
-                  name="fullname"
-                  ref="fullname"
-                  autocomplete="off"
-                />
-              </div>
-              <div class="form-group">
-                <input
-                  class="form-control form-control-solid h-auto py-7 px-6 rounded-lg font-size-h6"
-                  type="email"
-                  placeholder="Email"
-                  name="email"
-                  ref="remail"
-                  autocomplete="off"
-                />
-              </div>
-              <div class="form-group">
-                <input
-                  class="form-control form-control-solid h-auto py-7 px-6 rounded-lg font-size-h6"
-                  type="password"
-                  placeholder="Password"
-                  name="password"
-                  ref="rpassword"
-                  autocomplete="off"
-                />
-              </div>
-              <div class="form-group">
-                <input
-                  class="form-control form-control-solid h-auto py-7 px-6 rounded-lg font-size-h6"
-                  type="password"
-                  placeholder="Confirm password"
-                  name="cpassword"
-                  ref="cpassword"
-                  autocomplete="off"
-                />
-              </div>
-              <div class="form-group">
-                <label class="checkbox mb-0">
-                  <input type="checkbox" name="agree" />
-                  <span class="mr-2"></span>
-                  I Agree the
-                  <a href="#" class="ml-2">terms and conditions</a>.
-                </label>
-              </div>
-              <div class="form-group d-flex flex-wrap pb-lg-0 pb-3">
-                <button
-                  ref="kt_login_signup_submit"
-                  class="btn btn-primary font-weight-bolder font-size-h6 px-8 py-4 my-3 mr-4"
-                  style="width:150px;"
-                >
-                  Submit
-                </button>
-                <button
-                  type="button"
-                  id="kt_login_signup_cancel"
-                  class="btn btn-light-primary font-weight-bolder font-size-h6 px-8 py-4 my-3"
-                  @click="showForm('signin')"
-                >
-                  Cancel
-                </button>
-              </div>
-            </form>
-          </div>
-          <!--end::Signup-->
           <!--begin::Forgot-->
           <div class="login-form login-forgot">
             <!--begin::Form-->
@@ -265,21 +160,6 @@
           </div>
           <!--end::Forgot-->
         </div>
-        <!--begin::Content footer-->
-        <div
-          class="d-flex justify-content-lg-start justify-content-center align-items-end py-7 py-lg-0"
-        >
-          <a href="#" class="text-primary font-weight-bolder font-size-h5"
-            >Terms</a
-          >
-          <a href="#" class="text-primary ml-10 font-weight-bolder font-size-h5"
-            >Plans</a
-          >
-          <a href="#" class="text-primary ml-10 font-weight-bolder font-size-h5"
-            >Contact Us</a
-          >
-        </div>
-        <!--end::Content footer-->
       </div>
       <!--end::Content-->
     </div>
@@ -324,7 +204,7 @@ export default {
 
     backgroundImage() {
       return (
-        process.env.BASE_URL + "media/svg/illustrations/login-visual-1.svg"
+        process.env.BASE_URL + "media/svg/illustrations/working.svg"
       );
     }
   },
